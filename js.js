eb7b75d64b3c8184c9e0cb3964dcd48b071b1b32
justjjasper <98243819+justjjasper@users.noreply.github.com>
2022-01-22 21:57:35 -0800

    Update js.js

--- js.js
+++ js.js
@@ -7,5 +7,60 @@ const getUserChoice = userInput => {
   }
   }
   }
+  // console.log(getUserChoice('rock'))
   
-  console.log(getUserChoice('rock'))
+  function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3); {
+      switch(number) {
+        case 0:
+        return 'rock';
+        break;
+        case 1:
+        return 'paper';
+        break;
+        case 2: 
+        return 'scissors'
+        break;
+      }
+    }
+  }
+  
+  // console.log(getComputerChoice());
+  
+  const determineWinner = (userChoice,computerChoice) => {
+    if(userChoice === computerChoice) {
+      return 'The game is a tie!';
+    }
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    if (userChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+   if (userChoice === 'scissors') {
+     if (computerChoice === 'rock') {
+       return 'The computer won!';
+     } else {
+       return 'You won!';
+     }
+   }
+  }
+  
+  // console.log(determineWinner(getComputerChoice()))
+  
+  const playGame = () => {
+    let userChoice = getUserChoice('rock');
+    let computerChoice = getComputerChoice()
+    console.log('You threw: ' + userChoice);
+    console.log('The computer threw: ' + computerChoice);
+    console.log(determineWinner(userChoice,computerChoice))
+  }
+  playGame()
